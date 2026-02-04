131b3a44684c1df2a928c92c576580e349f48ea2
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-02-04 13:45:52 +0300

    Implement if-else logic to check if number is weird

---
+++ Python If-Else.py
@@ -0,0 +1,15 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 0 and n >= 6 and n <= 20 or n % 2 != 0 :
+        print("Weird")
+    else:
+        print("Not Weird")
