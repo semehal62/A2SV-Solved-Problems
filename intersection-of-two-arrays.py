12f19b319e8af345d5d3756ee4e273700aed1c3f
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-02-12 18:51:58 +0300

    Create intersection-of-two-arrays.py

---
+++ intersection-of-two-arrays.py
@@ -0,0 +1,6 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set_1 = set(nums1)
+        set_2 = set(nums2)
+        set_3 = set_1 & set_2
+        return (list(set_3))
