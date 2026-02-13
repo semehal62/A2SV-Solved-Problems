bd0a2cee0b1d2f8536f25578c60174ef7a09ae26
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-02-13 16:54:52 +0300

    Add function to count negative numbers in matrix
    Implement a solution to count negative numbers in a sorted matrix.

---
+++ count-negative-numbers-in-a-sorted-matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] < 0:
+                    count += n - j
+                    break
+
+        return count
