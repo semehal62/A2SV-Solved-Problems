6c9b0a92d4c2a1eb53e76c88bb0f0b271c208df4
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-02-26 16:24:45 +0300

    Update C_Number_of_equal.py

--- C_Number_of_equal.py
+++ C_Number_of_equal.py
@@ -1,38 +1,13 @@
+from collections import Counter
 l1,l2 = list(map(int,input().split()))
-
 arr1 = list(map(int,input().split()))
 arr2 = list(map(int,input().split()))
-
-ans = [0]*l1
+ 
+c_arr1 = Counter(arr1)
+c_arr2 = Counter(arr2)
 count = 0
-
-first = 0
-second = 0
-
-while first < l1 or second < l2:
-    if first != 0 and  arr1[first] == arr1[first-1]:
-        ans[first] = ans[first-1]
-        first += 1 
-
-    elif second < l2 and arr1[first] == arr2[second]:
-        while second < l2 and arr1[first] == arr2[second] :
-            count += 1
-            second += 1
-        ans[first] = count
-
-    else:
-        if  second < l2 and arr1[first] > arr2[second]:
-            second += 1
-        else:
-            first += 1
-
-
-
-
-    count = 0
-
-
-
-
-
-print(sum(ans))
+for key, value in c_arr1.items():
+    if key in c_arr2:
+        count += (value * c_arr2[key])
+ 
+print(count)
