772b922a7786848fe49cd590ac96ac75298dbd48
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-03-27 12:44:14 +0300

    Add solution for Permutations

---
+++ leetcode/0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def recur(i):
+            if i == len(nums):
+                res.append(nums[:])
+                return
+
+            for j in range(i,len(nums)):
+                nums[i], nums[j] = nums[j], nums[i]
+                recur(i+1)
+                nums[i], nums[j] = nums[j], nums[i]
+
+        recur(0)
+
+       
+
+        return res
+
+
