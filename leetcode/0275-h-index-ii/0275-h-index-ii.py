44eb38187cf2c0224ef2b475057d5508ad1c6670
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2026-04-06 19:26:18 +0300

    Add solution for H-Index II

--- leetcode/0275-h-index-ii/0275-h-index-ii.py
+++ leetcode/0275-h-index-ii/0275-h-index-ii.py
@@ -3,17 +3,15 @@ class Solution:
         # binary search
 
         n = len(citations)
-        low = 0
+        low = 1
         high = n
-        ans = 0
+        ans = 0 
         while low <= high:
             mid = (low + high) // 2
-            idx = bisect_left(citations, mid)
-            if n - idx >= mid:
+            print(mid)
+            if citations[n - mid] >= mid:
                 ans = mid
                 low = mid + 1
             else:
                 high = mid - 1 
-
-
         return ans
